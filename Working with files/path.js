9294b738c06a1f4f160d019e91336ceb495fd5fc
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-04-07 15:48:42 +0530

    Add files via upload

---
+++ Working with files/path.js
@@ -0,0 +1,6 @@
+import path from "path"
+console.log(path);
+let mypath="C:\\Users\\A\\web\\NodeJs\\Working with files\\pranali.txt"
+console.log(path.extname(mypath));
+console.log(path.dirname(mypath));
+
